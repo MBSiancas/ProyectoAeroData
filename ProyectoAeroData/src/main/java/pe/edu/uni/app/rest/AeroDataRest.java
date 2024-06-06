05419693262db5b6e70b1f640addde53c20ce3e5
MBSiancas <benjaminsiancas265@gmail.com>
2024-06-05 23:58:16 -0500

    Add files via upload

---
+++ ProyectoAeroData/src/main/java/pe/edu/uni/app/rest/AeroDataRest.java
@@ -0,0 +1,11 @@
+package pe.edu.uni.app.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/aerodata")
+@RestController
+
+public class AeroDataRest {
+
+}
---
+++ ProyectoAeroData/src/main/java/pe/edu/uni/app/service/AeroDataService.java
@@ -0,0 +1,5 @@
+package pe.edu.uni.app.service;
+
+public class AeroDataService {
+
+}
