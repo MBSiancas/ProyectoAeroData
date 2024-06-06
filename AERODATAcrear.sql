f6c28967100c8db04f2fe8bbd656ab940a330536
MBSiancas <benjaminsiancas265@gmail.com>
2024-06-05 23:51:21 -0500

    Update AERODATAcrear.sql

--- AERODATAcrear.sql
+++ AERODATAcrear.sql
@@ -4,23 +4,22 @@ use AERODATA
 
 create table AEROPUERTO(
 id_aeropuerto char(6) primary key not null,
-id_tipo char(2) not null,
 nombre_aeropuerto varchar(50) not null,
 ciudad varchar(30) not null,
 pais varchar(30) not null,
-num_compañias int not null,
+num_compaÃ±ias int not null,
 )
 
-create table COMPAÑIA(
-id_compañia char(6) primary key not null,
-nombre_compañia varchar(50) not null,
+create table COMPAÃ‘IA(
+id_compaÃ±ia char(6) primary key not null,
+nombre_compaÃ±ia varchar(50) not null,
 num_vuelos int not null,
 )
 
 create table VUELO(
 id_vuelo char(6) primary key not null,
 id_aeropuerto char(6) not null,
-id_compañia char(6) not null,
+id_compaÃ±ia char(6) not null,
 ciudad_origen varchar(30) not null,
 ciudad_destino varchar(30) not null,
 precio money not null,
@@ -36,20 +35,13 @@ num_pasaporte char(9) not null,
 nacionalidad varchar(30) not null,
 )
 
-create table TIPOAEROPUERTO(
-id_tipo char(2) primary key not null,
-tipo_aeropuerto varchar(20) not null
-)
-
-alter table AEROPUERTO
-add foreign key (id_tipo) references TIPOAEROPUERTO(id_tipo)
-
 alter table VUELO
 add foreign key (id_aeropuerto) references AEROPUERTO(id_aeropuerto)
 
 alter table VUELO
-add foreign key (id_compañia) references COMPAÑIA(id_compañia)
+add foreign key (id_compaÃ±ia) references COMPAÃ‘IA(id_compaÃ±ia)
 
 alter table PASAJERO
 add foreign key (id_vuelo) references VUELO(id_vuelo)
+
 
