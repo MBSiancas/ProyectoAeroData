c19dc8b34d80d1b2cba79a8a0363ecdb36a63506
MBSiancas <benjaminsiancas265@gmail.com>
2024-06-07 17:59:09 -0500

    Add files via upload

--- AERODATAcrear.sql
+++ AERODATAcrear.sql
@@ -6,16 +6,16 @@ go
 
 create table AEROPUERTO(
 id_aeropuerto char(6) primary key not null,
-nombre_aeropuerto varchar(50) not null,
+nombre varchar(50) not null,
 ciudad varchar(30) not null,
 pais varchar(30) not null,
-num_compaÃ±ias int not null,
+num_compañias int not null,
 )
 go
 
 create table EMPLEADO(
 id_empleado char(6) primary key not null,
-nombre_empleado varchar(150) not null,
+nombre varchar(150) not null,
 id_aeropuerto char(6) not null,
 fecha_contratacion datetime not null,
 salario money not null,
@@ -36,9 +36,9 @@ foreign key (id_aeropuerto) references AEROPUERTO(id_aeropuerto)
 );
 go
 
-create table COMPAÃ‘IA(
-id_compaÃ±ia char(6) primary key not null,
-nombre_compaÃ±ia varchar(50) not null,
+create table COMPAÑIA(
+id_compañia char(6) primary key not null,
+nombre varchar(50) not null,
 num_vuelos int not null,
 )
 go
@@ -46,7 +46,7 @@ go
 create table VUELO(
 id_vuelo char(6) primary key not null,
 id_aeropuerto char(6) not null,
-id_compaÃ±ia char(6) not null,
+id_compañia char(6) not null,
 ciudad_origen varchar(30) not null,
 ciudad_destino varchar(30) not null,
 fecha datetime not null,
@@ -54,13 +54,13 @@ precio money not null,
 num_max_pasajeros int not null,
 num_actu_pasajeros int not null,
 foreign key (id_aeropuerto) references AEROPUERTO(id_aeropuerto),
-foreign key (id_compaÃ±ia) references COMPAÃ‘IA(id_compaÃ±ia)
+foreign key (id_compañia) references COMPAÑIA(id_compañia)
 )
 go
 
 create table PASAJERO(
 id_pasajero char(6) primary key not null,
-nombre_pasajero varchar(150) not null,
+nombre varchar(150) not null,
 num_pasaporte char(9) not null,
 nacionalidad varchar(30) not null,
 num_vuelos int not null,
@@ -74,8 +74,15 @@ id_reserva char(6) primary key not null,
 id_pasajero char(6) not null,
 id_vuelo char(6) not null,
 fecha_reserva datetime not null,
+pago money,
 estado varchar(20) not null,
 foreign key (id_pasajero) REFERENCES PASAJERO(id_pasajero),
 foreign key (id_vuelo) REFERENCES VUELO(id_vuelo)
 )
 go
+
+
+
+
+
+
